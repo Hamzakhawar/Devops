98b1713d47f91614d9237d5b6ec319f188cb541d
hamza <hamzakhawar480@gmail.com>
2023-08-01 17:59:33 +0000

    new commands added

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 This is my first github file
 
 adding new commands using local machine (git)
+
+after clonning the code
+
+adding new commands using local machine
+
+git init
+git add 
+git commit
+git remote set-url --add newurl
+git remote set-url --delete oldurl
